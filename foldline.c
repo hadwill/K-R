41c48f76b2f1e3333884ea3af9c9b92d887e3353
hadwill <willame.hadrien@gmail.com>
2017-01-11 22:50:42 +0100

    Break main in two functions

--- foldline.c
+++ foldline.c
@@ -1,43 +1,71 @@
 #include <stdio.h>
 
-#define MAXLENGTH 81 /* 81 = 80 non-nl chars + '\n' + '\0' */
+#define MAXLENGTH 20 /* 81 = 80 non-nl chars + '\n' + '\0' */
 #define MAXLINE 1000
 #define IN 1
 #define OUT 0
 #define TAB 8
 
 int getline1(char line[], int maxline);
+void copy(char s[], int strl);
+
 char show[MAXLENGTH];
+
+
 
 int main()
 {
-  int len, i, j, k, col;
+  int len;
   char line[MAXLINE];
-  extern char show[MAXLENGTH];
 
-  col = 0;
   while((len =  getline1(line, MAXLINE)) > 0)
-    for(i = 0; i <= len-2; i = i + MAXLENGTH-1) {
-      for(j = 0; j <= MAXLENGTH; ++j)
-	show[j] = 0;
-      for(j = 0; j <= MAXLENGTH-2; ++j)
-        show[j] = line[i+j];
-      show[MAXLENGTH-1] = '\n';
-      show[MAXLENGTH] = '\0';
-      printf("%s", show);
-      /* printf("%d", len); */
-    }
+    copy(line, len);
+
   return 0;
 }
 
-  
+
+
+void copy(char s[], int l)
+{
+  int i, j, k, col, mod;
+
+  extern char show[MAXLENGTH];
+
+  col = 0;
+  for(i = 0; i <= l-2; i = i + col) {
+    for(j = 0; j <= MAXLENGTH; ++j)
+      show[j] = 0;
+    j = col = 0;
+    while(col <= MAXLENGTH-2) {
+      if (s[i+j] == '\t') {
+	mod = TAB - (col % TAB);
+	for (k = 0; k < mod; ++k) {
+	  show[col] = ' ';
+	  ++col;
+	}
+	++j;
+      }
+      else {
+	show[col] = s[i+j];
+	++col;
+	++j;
+      }
+    }
+    show[col] = '\n';
+    show[col + 1] = '\0';
+    printf("%s", show);
+  }
+}
+      
+
+
 int getline1(char s[], int lim)
 /*
    Returns the length of the line, stripped out of its trailing whitespace
-   It also replaces tabs with blanks.
 */
 {
-  int lastnw, c, i, state;
+  int lastnw, c, i, j, state;
   state = OUT;
   lastnw = 0;
   for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
